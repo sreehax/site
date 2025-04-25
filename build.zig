2a46cc140a34fcdd8e4774001908c619c50bbbfd
Sreehari Sreedev <me@ssree.dev>
2025-04-25 00:41:51 -0700

    github action

---
+++ build.zig
@@ -0,0 +1,10 @@
+const std = @import("std");
+const zine = @import("zine");
+
+pub fn build(b: *std.Build) !void {
+   b.getInstallStep().dependOn(&zine.website(b, .{}).step);
+
+   const serve = b.step("serve", "Start the Zine dev server");
+   const run_zine = zine.serve(b, .{});
+   serve.dependOn(&run_zine.step);
+}
